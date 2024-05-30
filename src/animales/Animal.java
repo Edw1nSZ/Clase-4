3b26923d954b44f9c21f9f20eaa66797b0d6cf5d
Edw1nSZ <edwincruz2basico@gmail.com>
2024-05-29 18:54:36 -0600

    prueba1

---
+++ src/animales/Animal.java
@@ -0,0 +1,27 @@
+package animales;
+
+public class Animal {
+    private int edad;
+    private String nombre;
+
+    public Animal(int edad, String nombre){
+        this.edad = edad;
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
